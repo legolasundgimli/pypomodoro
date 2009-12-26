70a8e24ab086f5802026dec4a7657810cabcc7ef
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2009-12-26 16:15:32 +0000

    First release

---
+++ pomodoro/src/conf/messages.py
@@ -0,0 +1,11 @@
+'''
+Created on Dec 17, 2009
+
+@author: uolter
+'''
+
+
+TASK_TOBE_DONE='Task to be done'
+TASK_INTERRUPTED='Task Interrupted'
+TASK_INPROGRESS='Task in Progress %d:%d'
+TASK_COMPLETED='Task Completed'
