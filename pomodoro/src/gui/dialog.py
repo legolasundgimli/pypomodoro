d7a18b921dfdf07aa5d2392fa8798bd36eaadef8
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2010-01-03 08:38:14 +0000

    new packate to deal with files refactorinng gui dialog new task attribute to identify saved task

---
+++ pomodoro/src/gui/dialog.py
@@ -0,0 +1,50 @@
+Created on Dec Jan, 2010
+
+@author: uolter
+
+
+class TaskDialog(wx.Dialog):
+    
+    def __init__(self, parent, id, title, tasklist):
+        wx.Dialog.__init__(self, parent, id, title, size=(500,200), style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER)    
+        # sizer =  self.CreateTextSizer('Tasks')
+        sizer=wx.BoxSizer(wx.HORIZONTAL)
+        sheet=TaskSheet(self, tasklist)        
+        sizer.Add(sheet, 0, wx.ALL, 1)        
+        self.SetSizer(sizer)
+    
+
+from wx.lib import sheet
+
+class TaskSheet(sheet.CSheet):
+    
+    def __init__(self, parent, tasklist):
+        sheet.CSheet.__init__(self, parent)
+        self.row = self.col = 0
+        self.tasklis=tasklist        
+        self.SetNumberRows(len(tasklist)+1)
+        self.SetNumberCols(3)
+                        
+        #Columns name:
+        self.SetColLabelValue(0, 'Task')
+        self.SetColLabelValue(1, 'Start')
+        self.SetColLabelValue(2, 'Stop')        
+        
+        self.loadTask()
+        
+    def loadTask(self):
+        
+                
+        row=0                
+        for item in self.tasklis:
+            self.SetCellValue(row, 0, item.name)
+            self.SetColSize(0, len(item.name.ljust(20))*8)            
+            self.SetCellValue(row, 1, item.startAt())
+            self.SetColSize(1, len(item.startAt())*8)            
+            if item.ended():
+                self.SetCellValue(row, 2, item.stopAt())
+                self.SetColSize(2, len(item.stopAt())*10)
+            else:
+                self.SetCellValue(row, 2, 'Running')
+            row+=1            
+        
