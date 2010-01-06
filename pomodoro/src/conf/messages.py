cfc3a1cb521cb598a8d5ee39227828f7108ed233
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2010-01-06 10:49:43 +0000

    export new label

--- pomodoro/src/conf/messages.py
+++ pomodoro/src/conf/messages.py
@@ -9,3 +9,13 @@ TASK_TOBE_DONE='Task to be done'
 TASK_INTERRUPTED='Task Interrupted'
 TASK_INPROGRESS='Task in Progress %d:%d'
 TASK_COMPLETED='Task Completed'
+
+
+BUTTON_START='Start'
+
+#################################
+DLG_TEXT_MESSAGE='Working one ?'
+DLG_TEXT_TITLE='Task Name'
+
+#################################
+DLG_TASK_TITLE='Task'
