d4bd4672a33e96e513f02dab435a7ef3f689d370
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2010-01-08 10:57:13 +0000

    Translations

--- trunk/pomodoro/src/conf/messages.py
+++ trunk/pomodoro/src/conf/messages.py
@@ -10,6 +10,21 @@ TASK_INTERRUPTED='Task Interrupted'
 TASK_INPROGRESS='Task in Progress %d:%d'
 TASK_COMPLETED='Task Completed'
 
+MENU_MENU="&Menu"
+MENU_MENU_RESET='&Reset'
+MENU_MENU_RESET_MSG="Reset Count"
+MENU_MENU_EXIT='E&xit'
+MENU_MENU_EXIT_MSG="Exit Tomato"
+
+MENU_TASK="&Task"
+MENU_TASK_RENAME='&Rename'
+MENU_TASK_RENAME_MSG='Rename Currnet Task' 
+MENU_TASK_VIEW='&View'
+MENU_TASK_VIEW_MSG='View All tasks'
+MENU_TASK_SAVE='&Save'
+MENU_TASK_SAVE_MSG='Save all tasks' 
+MENU_TASK_SEND='S&end'
+MENU_TASK_SEND_MSG='Send all task'
 
 BUTTON_START='Start'
 
@@ -19,3 +34,9 @@ DLG_TEXT_TITLE='Task Name'
 
 #################################
 DLG_TASK_TITLE='Task'
+DLG_TASK_BTN_CLOSE='Close'
+DLG_TASK_GRID_COL0 = 'Name'
+DLG_TASK_GRID_COL1 = 'Start'
+DLG_TASK_GRID_COL2 = 'Stop'
+
+DLG_TASK_GRID_TASK_RUNNING = 'Running'
