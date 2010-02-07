8ddecc60eec26533357161a6ece26f4f83292ba2
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2010-02-07 14:42:14 +0000

    no google account released

--- src/conf/settings.py
+++ src/conf/settings.py
@@ -4,10 +4,11 @@ Created on Dec 11, 2009
 @author: uolter
 '''
 
+# Minutes
 MAX_TIME=25
 
 ICON='ico.ico'
 
 CSV_SEPARATOR=','
 
-google_calendar_account='wtraspad@yahoo.it'
+google_calendar_account=''
