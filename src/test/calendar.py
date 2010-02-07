85409066762ec3e86d5d74a958a5ca6ef825e9e0
wtraspad <wtraspad@774d61d2-eedd-11de-a70e-4b51a9783938>
2010-02-07 14:46:18 +0000

    refactor

--- src/test/calendar.py
+++ src/test/calendar.py
@@ -13,7 +13,7 @@ from conf import settings
 class Test(unittest.TestCase):
 
 
-    def testTaskEvent(self):
+    def test1_TaskEvent(self):
         
         username=settings.google_calendar_account
         password=raw_input('Password for %s?' %username)
@@ -27,6 +27,25 @@ class Test(unittest.TestCase):
         event =entry.insert_single_event(mytask.name, mytask._startAt.timetuple(), mytask._stopAt.timetuple())
         
         self.assertFalse(event == None)
+        
+    def test2_TaskEvent(self):
+        
+        username=settings.google_calendar_account
+        password=raw_input('Password for %s?' %username)
+        
+        entry = EntryManager(username, password)
+        
+        my_task_list=task.MyTaskList()        
+        my_task_list.append('Google calendar')
+        time.sleep(120)
+        my_task_list.current.stop()
+        
+        tasks=my_task_list.task_to_send()
+        
+        for item in tasks:
+            event =entry.insert_single_event(item.name, item._startAt.timetuple(), item._stopAt.timetuple())
+            self.assertFalse(event == None)
+                
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testInstartEvent']
